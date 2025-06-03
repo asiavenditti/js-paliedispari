e054758ecbe0226841e4001ade193f9eb071d898
Your Name <you@example.com>
2025-06-03 16:55:51 +0200

    Add logic for pari or dispari game with user input and winner check

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function wordCheck(word) {
 
     // sfrutto il ciclo for per capovolgere la parola
     for (let i = word.length - 1; i >= 0; i--) {
-        let letter = word[i]
+        const letter = word[i]
         wordReverse = wordReverse + letter
 
 
@@ -38,3 +38,41 @@ else {
 
 
 /* Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto. */
+
+// creo una funzione che restituisca 'pari' o 'dispari'
+
+function checkNumb(numb) {
+
+    if (numb % 2 === 0) {
+        return 'pari'
+
+    } else {
+        return 'dispari'
+
+    }
+
+}
+
+// faccio dei prompt 
+
+const userChoice = prompt('pari o dispari?').toLocaleLowerCase()
+
+const userNumber = Number(prompt('Inserisci un numero'))
+console.log('Il tuo numero è', userNumber)
+
+const pcNumber = Math.floor(Math.random() * 5) + 1
+console.log('Il numero del computer è', pcNumber)
+
+// faccio la somma dei valori 
+const somma = userNumber + pcNumber
+
+// una volta ottenuta la somma, richiamo la funzione in una variabile "result"
+let result = checkNumb(somma)
+
+// faccio un confronto per dichiarare chi ha vinto 
+
+if (result === userChoice) {
+    console.log('Hai vinto!')
+} else {
+    console.log('Hai perso!')
+}
